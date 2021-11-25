e8566442024e2e1a0265a9d153e55aa6dd0fcff1
Kuter Dinel <kuterdinel@gmail.com>
2021-11-25 18:49:46 +0300

    Initial commit

---
+++ entries/test.md
@@ -0,0 +1,25 @@
+<script>
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
+</script>
+
+<script type="text/javascript" id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+</script>
+
+## This is a heading
+
+__ Making a bold statement here __
+
+regular text *Italic text*
+<p>
+When \(a \ne 0 \), there are two solutions to \(ax^2 + bx + c = 0\) and they are
+$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
+\(y = x^2 \over 2\)
+</p>
---
+++ main.py
@@ -0,0 +1,46 @@
+from datetime import date
+from jinja2 import Environment, FileSystemLoader
+from os import path
+import markdown
+import copy
+
+RENDERED_FOLDER = "result"
+ENTRIES_FOLDER = "entries"
+
+class BlogEntry:
+    def __init__(self, title, description, content, date):
+        self.title = title
+        self.description = description
+
+        content_file = open(path.join(ENTRIES_FOLDER, content))
+        self.content = markdown.markdown(content_file.read())
+        self.date = date.strftime("%d-%m-%Y")
+        self.url = title.lower().replace(' ', '-') + ".html"
+
+file_loader = FileSystemLoader("templates")
+env = Environment(loader=file_loader)
+
+def render_page(file_name, tmp_name, params):
+    template = env.get_template(tmp_name)
+    result = template.render(**params)
+    rfile = open(path.join(RENDERED_FOLDER, file_name), "w")
+    rfile.write(result)
+
+def render_all(general, entries):
+    index_info = copy.copy(general)
+    index_info["entries"] = entries
+
+    render_page("index.html", "index.html", index_info) 
+
+    for entry in entries:
+        entry_info = copy.copy(general)
+        entry_info["entry"] = entry
+        render_page(entry.url, "blog_entry.html", entry_info)
+
+entries = [
+    BlogEntry("Making a very simple static blog generator", "This is a description", "test.md", date.today()),
+    BlogEntry("A entry without description", "", "test.md", date.today()),
+    BlogEntry("Lorem ipsum", "Very descriptive", "lorem-ipsum.md", date.today()),
+]
+
+render_all({"author": "Replace Me"}, entries)
