8faa257a74aec29b98ae4ccc5ae9b4d4d5b45323
Kuter Dinel <kuterdinel@gmail.com>
2021-11-28 17:07:49 +0300

    Add multi-language support,
    * Added support for translating the inteerface.
    * Added support for having translated verisons of articles.
    * Added support for language specific articles.
    * Switch to utf-8 encoding

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from jinja2 import Environment, FileSystemLoader
 from os import path
 import markdown
 import copy
+from translation import LANGUAGES
 
 RENDERED_FOLDER = "result"
 ENTRIES_FOLDER = "entries"
+
+ACTIVE_LANGUAGES = ["en", "tr"]
+
+# Language configuration
+DEFAULT_LANGUAGE = "en"
 
 class BlogEntry:
     def __init__(self, title, description, content, date):
@@ -16,32 +22,89 @@ class BlogEntry:
         self.content = markdown.markdown(content_file.read(), extensions=['codehilite','fenced_code'])
         self.date = date.strftime("%d-%m-%Y")
         self.url = title.lower().replace(' ', '-') + ".html"
+        self.all_languages = [self]
+        self.language = DEFAULT_LANGUAGE
+
 
 file_loader = FileSystemLoader("templates")
 env = Environment(loader=file_loader)
 
 def render_page(file_name, tmp_name, params):
+    print("Rendering page: {}".format(file_name))
     template = env.get_template(tmp_name)
     result = template.render(**params)
     rfile = open(path.join(RENDERED_FOLDER, file_name), "w")
     rfile.write(result)
 
+def render_page_multilang(file_name, tmp_name, params, lang):
+    params = copy.copy(params)
+    params["languages"] = {x: LANGUAGES[x] for x in ACTIVE_LANGUAGES}
+    params["language"] = LANGUAGES[lang]
+    params["language_code"] = lang
+    params["local"] = LANGUAGES[lang]["localization"]
+
+    render_page(file_name, tmp_name, params)
+
 def render_all(general, entries):
-    index_info = copy.copy(general)
-    index_info["entries"] = entries
+    all_entries = []
+    entries_per_lang = {lang: [] for lang in ACTIVE_LANGUAGES}
 
-    render_page("index.html", "index.html", index_info) 
-
+    # Process entries and set alternative versions.
     for entry in entries:
+        if isinstance(entry, BlogEntry):
+            all_entries.append(entry)
+            entry.language = DEFAULT_LANGUAGE
+            entries_per_lang[DEFAULT_LANGUAGE].append(entry)
+            continue
+
+        all_languages = entry.values()
+        # Handle multi language entries.
+        for lang in entry:
+            entry[lang].language = lang
+            entry[lang].all_languages = all_languages
+            all_entries.append(entry[lang])
+            entries_per_lang[lang].append(entry[lang])
+
+
+    general["active_languages"] = ACTIVE_LANGUAGES
+    # Render the index pages for each active language.
+    for lang in ACTIVE_LANGUAGES:
+        index_info = copy.copy(general)
+        index_info["entries"] = entries_per_lang[lang]
+        index_info["all_entries"] = all_entries
+        render_page_multilang(LANGUAGES[lang]["index"], "index.html", index_info, lang)
+
+    def render_blog_entry(entry):
         entry_info = copy.copy(general)
         entry_info["entry"] = entry
-        render_page(entry.url, "blog_entry.html", entry_info)
+        render_page_multilang(entry.url, "blog_entry.html", entry_info, entry.language)
+
+    for entry in all_entries:
+        render_blog_entry(entry)
+
+def process_languages():
+    for lang in LANGUAGES:
+        LANGUAGES[lang]["code"] = lang
+
+        index_suffix = ""
+        if DEFAULT_LANGUAGE != lang:
+            index_suffix = "-" + lang
+        LANGUAGES[lang]["index"] = "index{}.html".format(index_suffix)
+process_languages()
+
+
+# ------Add-your-blog-posts-here------
 
 entries = [
-    BlogEntry("Making a very simple static blog generator", "This is a description", "test.md", date.today()),
+    {"en": BlogEntry("Making a very simple static blog generator", "This is a description", "test.md", date.today()),
+     "tr": BlogEntry("Basic bir static blog oluşturucusu yapmak", "Bu bir açıklama", "test-tr.md", date.today())},
     BlogEntry("A entry without description", "", "test.md", date.today()),
     BlogEntry("Lorem ipsum", "Very descriptive", "lorem-ipsum.md", date.today()),
     BlogEntry("Code highlight test", "Testing if we can highlight code in markdown", "code_test.md", date.today())
 ]
 
+
+# Replace your name here.
 render_all({"author": "Replace Me"}, entries)
+
+
---
+++ translation.py
@@ -0,0 +1,26 @@
+LANGUAGES = {
+    "en": {
+        "name": "English",
+        "localization": {
+            "headline": "{author}'s blog",
+            "blog_date_author": "on <strong>{date}</strong> by <strong>{author}</strong>.",
+            "blog_entries": "Blog entries",
+            "personal_information": "Personal information",
+            "go_back": "Go back",
+            "other_languages": "Other languages"
+        }
+    },
+    "tr": {
+        "name": "Türkçe",
+        "localization": {
+            "headline": "{author}'in bloğu",
+            "blog_date_author": "<strong>{date}</strong>'da <strong>{author}</strong> tarafından",
+            "blog_entries": "Blog yazıları",
+            "personal_information": "İletişim",
+            "go_back": "Geri git",
+            "other_languages": "Diğer diller"
+        }
+    }
+}
+
+
